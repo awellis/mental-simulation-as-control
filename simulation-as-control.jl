cbc48030a6aecca0c699d1be27f2bf0631120b2d
Andrew Ellis <andrewellis@pm.me>
2022-06-15 15:50:06 +0200

    Add pluto presentation

---
+++ simulation-as-control.jl
@@ -0,0 +1,37 @@
+### A Pluto.jl notebook ###
+# v0.19.8
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ fde7f730-eca3-11ec-11fd-a3669efbfb62
+md"""
+
+# Simulation as a control problem
+
+"""
+
+# ╔═╡ 7b82b21f-dd19-4f6b-89a4-917f3c7ad770
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.8.0-rc1"
+manifest_format = "2.0"
+project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═fde7f730-eca3-11ec-11fd-a3669efbfb62
+# ╠═7b82b21f-dd19-4f6b-89a4-917f3c7ad770
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
